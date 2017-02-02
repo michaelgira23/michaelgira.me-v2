0ba522830ab5b4daa517fa382a5fe4b73ffb4095
Michael Gira <contact@michaelgira.me>
2017-02-02 10:59:00 -0600

    Add EJS and render data stored in JSON

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@
 
 const port = 2468;
 
+const me = require(__dirname + '/me.json');
+
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 // Static Files
 app.use('/', express.static(__dirname + '/public'));
 
 // Main site
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/html/index.html');
+	res.render('pages/index', me);
 });
 
 // Redirect all directories to the main
