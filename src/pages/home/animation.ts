b540d33d0aa229f70d360d31056e59f573680433
Michael Gira <contact@michaelgira.me>
2018-07-16 18:52:13 -0500

    Separate into own `Swipe` class

--- src/pages/home/animation.ts
+++ src/pages/home/animation.ts
@@ -1,16 +1,18 @@
+import { Swipe, SwipeConfig } from './swipe';
+
+import anime from 'animejs';
 import { distance } from '../../shared/utils';
-import animejs from 'animejs';
-// Fix cause Typescript doesn't think there's a default import for animejs
-const anime: (params: anime.AnimeParams) => anime.AnimeInstance = animejs;
-
-// declare function anime(params: anime.AnimeParams): anime.AnimeInstance;
-
 const svgns = 'http://www.w3.org/2000/svg';
 const svgViewboxWidth = 100;
 const svgViewboxHeight = 100;
-const svg = document.querySelector('.animation-overlay');
+const svg = document.querySelector('.animation-overlay') as Element;
 
-const swipes = [
+interface SwipeSequence extends SwipeConfig {
+	start: number;
+	delay: number;
+}
+
+const swipeColors = [
 	'#ffffff',
 	// '#fcecee',
 	// '#f9d9dd',
@@ -23,22 +25,56 @@ const swipes = [
 	// '#e75568',
 	'#e44257',
 	// '#e23047',
-	'#ce2c41',
+	'#ce2c41'
 	// '#b9283b',
 	// '#a52334',
 	// '#901f2e',
 	// '#7c1b27'
 ].reverse();
 
+const swipes: SwipeSequence[] = [
+	{
+		start: 0,
+		delay: 1000,
+		color: '#ce2c41'
+	},
+	{
+		start: 250,
+		delay: 1000,
+		color: '#e44257'
+	},
+	{
+		start: 350,
+		delay: 1000,
+		color: '#e96879'
+	},
+	{
+		start: 450,
+		delay: 1000,
+		color: '#ffffff'
+	}
+];
+
+// for (const swipeConfig of swipes) {
+// 	const swipe = new Swipe(swipeConfig);
+// 	setTimeout(() => {
+		// swipe.swipeMid();
+
+		// setTimeout(() => {
+		// 	swipe.swipeOut();
+		// }, swipeConfig.delay);
+// 	}, swipeConfig.start);
+// }
+
 swipeLoop(0);
 function swipeLoop(i: number) {
 	if (i < swipes.length) {
-		swipe(swipes[i]);
+		swipef(swipeColors[i]);
 		setTimeout(() => swipeLoop(++i), 250);
 	}
 }
 
-function swipe(color = '#e23047', heightPercent = 1) {
+function swipef(color = '#e23047', heightPercent = 1) {
 	const diagonalWidth = distance(0, 0, svgViewboxWidth, svgViewboxHeight);
 	const radialWidth = distance(0, 0, svgViewboxWidth / 2, svgViewboxHeight / 2);
 
---
+++ src/pages/home/swipe.ts
@@ -0,0 +1,90 @@
+import anime from 'animejs';
+import { distance } from '../../shared/utils';
+
+const svgns = 'http://www.w3.org/2000/svg';
+const svgViewboxWidth = 100;
+const svgViewboxHeight = 100;
+const svg = document.querySelector('.animation-overlay') as Element;
+
+const defaultSwipeOptions = {
+	heightPercent: 1,
+	easeDuration: 1000,
+	easeFunction: 'easeInOutExpo'
+};
+
+export interface SwipeConfig {
+	color: string;
+	heightPercent?: number;
+	easeDuration?: number;
+	easeFunction?: string;
+}
+
+export class Swipe {
+
+	color: string;
+	heightPercent: number;
+	easeDuration: number;
+	easeFunction: string;
+
+	rect: SVGRectElement;
+
+	width: number;
+	height: number;
+
+	xFrom: number;
+	yFrom: number;
+
+	yMid: number;
+	yTo: number;
+
+	constructor(config: SwipeConfig) {
+		config = Object.assign({}, defaultSwipeOptions, config);
+		this.color = config.color;
+		this.heightPercent = config.heightPercent as number;
+		this.easeDuration = config.easeDuration as number;
+		this.easeFunction = config.easeFunction as string;
+
+		const diagonalWidth = distance(0, 0, svgViewboxWidth, svgViewboxHeight);
+		// const radialWidth = distance(0, 0, svgViewboxWidth / 2, svgViewboxHeight / 2);
+
+		this.width = diagonalWidth;
+		this.height = diagonalWidth * this.heightPercent;
+
+		this.rect = document.createElementNS(svgns, 'rect');
+		this.xFrom = svgViewboxWidth - (this.width / 2);
+		this.yFrom = svgViewboxHeight;
+		this.yMid = this.yFrom - diagonalWidth;
+		this.yTo = this.yMid - diagonalWidth;
+
+		this.setAttribute('x', this.xFrom);
+		this.setAttribute('y', this.yMid);
+		this.setAttribute('width', this.width);
+		this.setAttribute('height', this.height);
+		this.setAttribute('transform', `rotate(-45, ${svgViewboxWidth}, ${svgViewboxHeight})`);
+		this.setAttribute('fill', this.color);
+
+		svg.appendChild(this.rect);
+	}
+
+	swipeMid() {
+		this.animate(this.yMid);
+	}
+
+	swipeOut() {
+		this.animate(this.yTo);
+	}
+
+	private animate(y: number) {
+		anime({
+			targets: this.rect,
+			y,
+			duration: this.easeDuration,
+			easing: 'easeInOutExpo'
+		});
+	}
+
+	private setAttribute(name: string, value: string | number) {
+		this.rect.setAttributeNS(svgns, name, value.toString());
+	}
+
+}
