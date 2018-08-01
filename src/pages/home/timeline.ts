e00a5b47c37a667a88a41d6cd3aeeb2d7a85bd72
Michael Gira <contact@michaelgira.me>
2018-08-01 15:41:51 -0600

    Show content after swipe animation

--- src/pages/home/timeline.ts
+++ src/pages/home/timeline.ts
@@ -1,35 +1,66 @@
 import anime from 'animejs';
+import { Swipe, SwipeConfig } from './swipe';
 
-const icon = document.querySelector('.icon') as SVGElement;
-const outline = document.querySelector('.icon #outline') as SVGElement;
-const fill = document.querySelector('.icon #fill-black') as SVGStopElement;
+interface SwipeSequence extends SwipeConfig {
+	start: number;
+	delay: number;
+}
 
-const titleName = document.querySelector('.title .name');
-const titleDesc = document.querySelector('.title .desc');
+const swipes: SwipeSequence[] = [
+	{
+		start: 0,
+		delay: 1000,
+		color: '#ce2c41'
+	},
+	{
+		start: 250,
+		delay: 1000,
+		color: '#e44257'
+	},
+	{
+		start: 350,
+		delay: 1000,
+		color: '#e96879'
+	// },
+	// {
+	// 	start: 450,
+	// 	delay: 1000,
+	// 	color: '#ffffff'
+	}
+];
+
+const $body = document.body;
+
+const $icon = document.querySelector('.icon') as SVGElement;
+const $outline = document.querySelector('.icon #outline') as SVGElement;
+const $fill = document.querySelector('.icon #fill-black') as SVGStopElement;
+
+const $titleName = document.querySelector('.title .name');
+const $titleDesc = document.querySelector('.title .desc');
 
 const timeline = anime.timeline();
 timeline
 	.add({
-		targets: outline,
+		targets: $outline,
 		duration: 1000,
 		easing: 'easeInOutSine',
 		strokeDashoffset: [anime.setDashoffset, 0]
 	})
 	.add({
-		targets: fill,
+		targets: $fill,
 		duration: 500,
 		easing: 'easeInOutExpo',
 		y: 1
 	})
 	.add({
-		targets: icon,
+		targets: $icon,
 		duration: 500,
 		easing: 'easeInOutExpo',
 		translateX: ['50%', 0]
 	})
 	.add({
 		offset: '-=500',
-		targets: titleName,
+		targets: $titleName,
 		duration: 500,
 		easing: 'easeInOutExpo',
 		translateX: ['5rem', 0],
@@ -37,9 +68,25 @@ timeline
 	})
 	.add({
 		offset: '-=450',
-		targets: titleDesc,
+		targets: $titleDesc,
 		duration: 500,
 		easing: 'easeInOutExpo',
 		translateX: ['5rem', 0],
 		opacity: 1
 	});
+
+timeline.complete = () => {
+	console.log('All done!');
+
+	for (const swipeConfig of swipes) {
+		const swipe = new Swipe(swipeConfig);
+		setTimeout(() => {
+			swipe.swipeMid();
+
+			setTimeout(() => {
+				$body.classList.add('resume');
+				swipe.swipeOut();
+			}, swipeConfig.delay);
+		}, swipeConfig.start);
+	}
+};
